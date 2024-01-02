bf460e4248a7887750b8161581ed959ebc27abe8
Ewald Arnold <github@ewald-arnold.de>
2024-01-02 12:17:57 +0100

    added linux support files

---
+++ build-linux.sh
@@ -0,0 +1,12 @@
+~/.local/bin/pyinstaller --onefile fktdeepl.py
+~/.local/bin/pyinstaller --onefile fktsplit2.py
+~/.local/bin/pyinstaller --onefile fktsplit3.py
+
+mkdir -p $HOME/tmp/fktdeepl
+
+cp dist/fktdeepl  $HOME/tmp/fktdeepl 
+cp dist/fktsplit2 $HOME/tmp/fktdeepl 
+cp dist/fktsplit3 $HOME/tmp/fktdeepl
+cp test.sh        $HOME/tmp/fktdeepl 
+cp testfile2.txt  $HOME/tmp/fktdeepl 
+
